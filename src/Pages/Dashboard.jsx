8c3f9d91849e3416c5614c893b846ca50b5a3985
Siddharth Singh Kushwaha <siddharthmnbv@gmail.com>
2023-11-08 11:59:42 +0530

    mark complete

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -10,7 +10,7 @@ import { useCallback } from "react";
 
 const Dashboard = () => {
   useCourseList();
-  const {isloading} = useAuthentication("/dashboard")
+  const { isloading } = useAuthentication("/dashboard");
   const allCourses = useSelector((state) => state.courseList);
   const user = useSelector((state) => state.user);
   const [userCourseData, setUserCourseData] = useState(null);
@@ -22,10 +22,10 @@ const Dashboard = () => {
       .select("course")
       .eq("user_id", user.id);
     let array = data[0]?.course;
-    if(array.length === 0){
+    if (array.length === 0) {
       setPageData(true);
       return;
-    }else{
+    } else {
       setPageData(false);
     }
 
@@ -43,7 +43,7 @@ const Dashboard = () => {
   }, [user.id, allCourses, setUserCourseData]);
 
   useEffect(() => {
-    if(user.id !== ""){
+    if (user.id !== "") {
       fetchCourseData();
     }
   }, [user.id, fetchCourseData]);
@@ -51,63 +51,83 @@ const Dashboard = () => {
   return (
     <div>
       <Navbar />
-      {pageData ? <div className="flex justify-center align-middle">No History, Plaese Enroll in a course</div> :<>
-      {isloading || userCourseData === null ? <div className="flex justify-center align-middle">Loading enrolled Courses...</div>
-      :<div className="p-12">
-        <div className="flex items-center mb-8">
-          <div className="mr-4">
-            <img
-              src={user.image}
-              alt="User Name"
-              className="w-16 h-16 rounded-full object-cover"
-            />
-          </div>
-          <div>
-            <h1 className="text-2xl font-semibold">{user.username}</h1>
-            <p className="text-gray-600">{user.email}</p>
-          </div>
+      {pageData ? (
+        <div className="flex justify-center align-middle">
+          No History, Plaese Enroll in a course
         </div>
-        <h1 className="text-2xl font-semibold">My Enrolled Courses</h1>
-        <ul className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-          {userCourseData?.map((course) => (
-            <li key={course.id} className="border border-black rounded-md p-4">
-              {/* <img
+      ) : (
+        <>
+          {isloading || userCourseData === null ? (
+            <div className="flex justify-center align-middle">
+              Loading enrolled Courses...
+            </div>
+          ) : (
+            <div className="p-12">
+              <div className="flex items-center mb-8">
+                <div className="mr-4">
+                  <img
+                    src={user.image}
+                    alt="User Name"
+                    className="w-16 h-16 rounded-full object-cover"
+                  />
+                </div>
+                <div>
+                  <h1 className="text-2xl font-semibold">{user.username}</h1>
+                  <p className="text-gray-600">{user.email}</p>
+                </div>
+              </div>
+              <h1 className="text-2xl font-semibold">My Enrolled Courses</h1>
+              <ul className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+                {userCourseData?.map((course) => (
+                  <li
+                    key={course.id}
+                    className="border border-black rounded-md p-4"
+                  >
+                    {/* <img
                 src={course.thumbnail}
                 alt={course.name}
                 className="w-full h-32 object-cover rounded-md"
               /> */}
-              <h2 className="mt-2 text-xl font-semibold">
-                {course.name}
-              </h2>
-              <p className="text-gray-600">{course.instructor}</p>
-              <p className="text-gray-600">Due Date: 19/01/2024</p>
-              <div className="mt-4">
-                <div className="relative pt-1">
-                  <div className="flex mb-2 items-center justify-between">
-                    <div>
-                      <span className="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-teal-600 bg-teal-200">
-                        Progress
-                      </span>
+                    <div className="flex justify-between">
+                      <h2 className="mt-2 text-xl font-semibold">
+                        {course.name}
+                      </h2>
+                      <div className="flex flex-col justify-end text-right">
+                        <input className="ms-10" type="checkbox"></input>
+                        <p className="text-xs">Mark <br/>complete</p>
+                      </div>
                     </div>
-                    <div className="text-right">
-                      <span className="text-xs font-semibold inline-block text-purple-500">
-                        50% complete
-                      </span>
+                    <p className="text-gray-600">{course.instructor}</p>
+                    <p className="text-gray-600">Due Date: 19/01/2024</p>
+                    <div className="mt-4">
+                      <div className="relative pt-1">
+                        <div className="flex mb-2 items-center justify-between">
+                          <div>
+                            <span className="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-teal-600 bg-teal-200">
+                              Progress
+                            </span>
+                          </div>
+                          <div className="text-right">
+                            <span className="text-xs font-semibold inline-block text-purple-500">
+                              50% complete
+                            </span>
+                          </div>
+                        </div>
+                        <div className="overflow-hidden h-2 mb-4 text-xs flex rounded bg-teal-200">
+                          <div
+                            style={{ width: "50%" }}
+                            className="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-teal-500"
+                          ></div>
+                        </div>
+                      </div>
                     </div>
-                  </div>
-                  <div className="overflow-hidden h-2 mb-4 text-xs flex rounded bg-teal-200">
-                    <div
-                      style={{ width: "50%" }}
-                      className="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-teal-500"
-                    ></div>
-                  </div>
-                </div>
-              </div>
-            </li>
-          ))}
-        </ul>
-      </div>}
-      </>}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          )}
+        </>
+      )}
     </div>
   );
 };
